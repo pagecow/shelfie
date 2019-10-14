e9340acb402b18dad572d189a37a4d27c307ccba
pagecow <pagecow@gmail.com>
2019-10-14 06:39:13 -0600

    getting closer

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -8,22 +8,22 @@ class Product extends React.Component {
             image1: 'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Champion-Men-s-Rally-Pro-Black-%26-White-Shoes-_298256.jpg',
             productName1: 'Shoes',
             price1: '$49',
-            showInput: false,
+            showInput1: false,
 
             image2: 'https://www.rei.com/media/product/878986',
             productName2: 'Shirt',
             price2: '$27',
-            showInput: false,
+            showInput2: false,
 
             image3: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAhhSgEvAn-T4m9aLTsFMBUQQ2xXsBZSpz6qFhF9XLS2hQgMC7hg',
             productName3: 'hat',
             price3: '$17',
-            showInput: false,
+            showInput3: false,
 
             image4: '',
             productName4: '',
             price4: '',
-            showInput: false,
+            showInput4: false,
         }
     }
 
@@ -55,16 +55,100 @@ class Product extends React.Component {
 
     }
 
-    showInput = () => {
-        if(this.state.showInput === false) {
+    showInput1 = () => {
+        if(this.state.showInput1 === false) {
             return this.setState({
-                showInput: true
+                showInput1: true
             })
         } else {
             return this.setState({
-                showInput: false
+                showInput1: false
             })
         }
+    }
+
+    showInput2 = () => {
+        if(this.state.showInput2 === false) {
+            return this.setState({
+                showInput2: true
+            })
+        } else {
+            return this.setState({
+                showInput2: false
+            })
+        }
+    }
+
+    showInput3 = () => {
+        if(this.state.showInput3 === false) {
+            return this.setState({
+                showInput3: true
+            })
+        } else {
+            return this.setState({
+                showInput3: false
+            })
+        }
+    }
+
+    showInput4 = () => {
+        if(this.state.showInput4 === false) {
+            return this.setState({
+                showInput4: true
+            })
+        } else {
+            return this.setState({
+                showInput4: false
+            })
+        }
+    }
+
+    handleProductNameChange1 = (value) => {
+        this.setState({
+            productName1: value
+        })
+    }
+
+    handlePriceChange1 = (value) => {
+        this.setState({
+            price1: value
+        })
+    }
+
+    handleProductNameChange2 = (value) => {
+        this.setState({
+            productName2: value
+        })
+    }
+
+    handlePriceChange2 = (value) => {
+        this.setState({
+            price2: value
+        })
+    }
+
+    handleProductNameChange3 = (value) => {
+        this.setState({
+            productName3: value
+        })
+    }
+
+    handlePriceChange3 = (value) => {
+        this.setState({
+            price3: value
+        })
+    }
+
+    handleProductNameChange4 = (value) => {
+        this.setState({
+            productName4: value
+        })
+    }
+
+    handlePriceChange4 = (value) => {
+        this.setState({
+            price4: value
+        })
     }
 
 
@@ -96,16 +180,25 @@ class Product extends React.Component {
 
                         <div className='rb-right-stuff'>
                             <p className='rb-words'>{productName1}</p>
+                            {this.state.showInput1 && (<input placeholder="New product name..." onChange={e => this.handleProductNameChange1(e.target.value)}/>)}
+                            
                             <p className='rb-words'>{price1}</p>
+                            {this.state.showInput1 && (<input placeholder="New price..." onChange={e => this.handlePriceChange1(e.target.value)}/>)}
 
                             <br/>
                             <br/>
                             <br/>
-                            <br/>
+                            
 
                             <div className='rb-buttons'>
-                                <button className='rb-button'>Delete</button>   
-                                <button className='rb-button'>Edit</button>  
+                                <button className='rb-button' onClick={this.handleDeleteRequest}>Delete</button>   
+                                <button className='rb-button'
+                                onClick={this.showInput1}
+                                >Edit</button>
+                                
+                                {this.state.showInput1 && (<button className='rb-button' onClick={this.showInput1}>Make Change</button>)}  
+
+
                             </div>
 
                         </div>
@@ -116,16 +209,22 @@ class Product extends React.Component {
 
                         <div className='rb-right-stuff'>
                             <p className='rb-words'>{productName2}</p>
+                            {this.state.showInput2 && (<input placeholder="New product name..." onChange={e => this.handleProductNameChange2(e.target.value)}/>)}
+                            
                             <p className='rb-words'>{price2}</p>
+                            {this.state.showInput2 && (<input placeholder="New price..." onChange={e => this.handlePriceChange2(e.target.value)}/>)}
 
                             <br/>
                             <br/>
                             <br/>
-                            <br/>
+                            
 
                             <div className='rb-buttons'>
-                                <button className='rb-button'>Delete</button>   
-                                <button className='rb-button'>Edit</button>  
+                                <button className='rb-button' onClick={this.handleDeleteRequest}>Delete</button>   
+                                <button className='rb-button'
+                                onClick={this.showInput2}
+                                >Edit</button>
+                                {this.state.showInput2 && (<button className='rb-button' onClick={this.showInput2}>Make Change</button>)}   
                             </div>
 
                         </div>
@@ -136,16 +235,22 @@ class Product extends React.Component {
 
                         <div className='rb-right-stuff'>
                             <p className='rb-words'>{productName3}</p>
+                            {this.state.showInput3 && (<input placeholder="New product name..." onChange={e => this.handleProductNameChange3(e.target.value)}/>)}
+                            
                             <p className='rb-words'>{price3}</p>
+                            {this.state.showInput3 && (<input placeholder="New price..." onChange={e => this.handlePriceChange3(e.target.value)}/>)}
 
                             <br/>
                             <br/>
                             <br/>
-                            <br/>
+                            
 
                             <div className='rb-buttons'>
-                                <button className='rb-button'>Delete</button>   
-                                <button className='rb-button'>Edit</button>  
+                                <button className='rb-button' onClick={this.handleDeleteRequest}>Delete</button>   
+                                <button className='rb-button'
+                                onClick={this.showInput3}
+                                >Edit</button>  
+                                {this.state.showInput3 && (<button className='rb-button' onClick={this.showInput3}>Make Change</button>)} 
                             </div>
 
                         </div>
@@ -156,21 +261,22 @@ class Product extends React.Component {
 
                         <div className='rb-right-stuff'>
                             <p className='rb-words'>{productName4}</p>
-                            {this.state.showInput && (<input placeholder="New product name..."/>)}
+                            {this.state.showInput4 && (<input placeholder="New product name..." onChange={e => this.handleProductNameChange4(e.target.value)}/>)}
                             
                             <p className='rb-words'>${price4}</p>
-                            {this.state.showInput && (<input placeholder="New price..."/>)}
+                            {this.state.showInput4 && (<input placeholder="New price..." onChange={e => this.handlePriceChange4(e.target.value)}/>)}
 
                             <br/>
                             <br/>
                             <br/>
-                            <br/>
+                            
 
                             <div className='rb-buttons'>
                                 <button className='rb-button' onClick={this.handleDeleteRequest}>Delete</button>   
                                 <button className='rb-button'
-                                onClick={this.showInput}
-                                >Edit</button>  
+                                onClick={this.showInput4}
+                                >Edit</button> 
+                                {this.state.showInput4 && (<button className='rb-button' onClick={this.showInput4}>Make Change</button>)}  
                             </div>
 
                         </div>
